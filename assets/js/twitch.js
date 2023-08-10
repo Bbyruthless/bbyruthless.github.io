4775c83d541a365828b0ccc0c63238202cb5e51a
Siqsaw <120989099+siqsaw@users.noreply.github.com>
2023-08-10 13:06:30 +0100

    Rebrand (#2)
    * Rework complete minus new copy
    
    * Fixed calc layout and force credit links to open in new windows
    
    * Added links page, home icon/link and removed old file
    
    * Updated links page and added back link
    
    * Small restructure to folder structure and added copy
    
    * Finalised Twitch player

---
+++ assets/js/twitch.js
@@ -0,0 +1,32 @@
+var player = new Twitch.Player('twitch-embed', {
+    channel: 'bbyruthless',
+    width: 400,
+    height: 225,
+    parent: [
+        'github.io',
+        'bbyruthless.com'
+    ]
+});
+
+var twitchElement = document.getElementById('twitch-content');
+
+var streamOnline = function () {
+    twitchElement.classList.remove('hidden');
+    setTimeout(function () {
+        twitchElement.classList.remove('visuallyhidden');
+    }, 20);
+};
+
+var streamOffline = function () {
+    twitchElement.classList.add('visuallyhidden');
+    twitchElement.addEventListener('transitionend', function (e) {
+        twitchElement.classList.add('hidden');
+    }, {
+        capture: false,
+        once: true,
+        passive: false
+    });
+};
+
+player.addEventListener(Twitch.Player.PLAYING, streamOnline);
+player.addEventListener(Twitch.Player.ENDED, streamOffline);
